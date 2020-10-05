c97646b10a8cee507f1caa3d2f487a3955678b7e
João Pedro <joaopedrosanchez302@gmail.com>
2020-10-05 15:56:52 -0300

    Posição scroll animação alterada

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 import ScrollSuave from './modules/scroll-suave.js';
-import initAnimacaoScroll from './modules/scroll-animacao.js';
 import initAccordion from './modules/accordion.js';
 import initTabNav from './modules/tabnav.js';
 import initModal from './modules/modal.js';
@@ -9,11 +8,11 @@ import initMenuMobile from './modules/menu-mobile.js';
 import initFuncionamento from './modules/funcionamento.js';
 import initFecthAnimais from './modules/fetch-animais.js';
 import initFetchBiticoin from './modules/fetch-bitcoin.js';
+import initAnimacaoScroll from './modules/scroll-animacao.js';
 
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
 scrollSuave.init();
 
-initAnimacaoScroll();
 initAccordion();
 initTabNav();
 initModal();
@@ -23,3 +22,4 @@ initMenuMobile();
 initFuncionamento();
 initFecthAnimais();
 initFetchBiticoin();
+initAnimacaoScroll();
