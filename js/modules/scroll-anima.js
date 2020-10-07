f627e2dbaa5250822e51404ef26118f9c1a9c4bd
João Pedro <joaopedrosanchez302@gmail.com>
2020-10-07 12:09:23 -0300

    Animação ao scroll refeita

--- js/modules/scroll-anima.js
+++ js/modules/scroll-anima.js
@@ -2,21 +2,36 @@ export default class ScrollAnima {
   constructor(sections) {
     this.sections = document.querySelectorAll(sections);
     this.halfWindow = window.innerHeight * 0.6;
-    this.animaScroll = this.animaScroll.bind(this);
+    this.checkDistance = this.checkDistance.bind(this);
   }
 
-  animaScroll() {
-    this.sections.forEach((section) => {
-      const sectionTop = section.getBoundingClientRect().top;
-      const isSectionVisible = sectionTop - this.halfWindow < 0;
-      if (isSectionVisible) {
-        section.classList.add('ativo');
+  getDistance() {
+    this.distance = [...this.sections].map((section) => ({
+      element: section,
+      offset: Math.floor(section.offsetTop - this.halfWindow),
+    }));
+  }
+
+  checkDistance() {
+    this.distance.forEach((item) => {
+      if (window.pageYOffset > item.offset) {
+        item.element.classList.add('ativo');
+      } else if (item.element.classList.contains('ativo')) {
+        item.element.classList.remove('ativo');
       }
     });
   }
 
   init() {
-    window.addEventListener('scroll', this.animaScroll);
+    if (this.sections.length) {
+      this.getDistance();
+      this.checkDistance();
+      window.addEventListener('scroll', this.checkDistance);
+    }
     return this;
+  }
+
+  stop() {
+    window.removeEventListener('scroll', this.checkDistance);
   }
 }
